36599cdb872902fbe636bfb74cbbdb6a7087bec4
wiehoang <ndhhoang1210@gmail.com>
2024-01-15 20:47:13 +0700

    Optimized statement and added comments

--- megacorp_bonus.py
+++ megacorp_bonus.py
@@ -4,13 +4,14 @@ Given an array representing a line of seats of employees at MegaCorp, determine 
 For example, given [10, 40, 200, 1000, 60, 30], you should return [1, 2, 3, 4, 2, 1].'''
 
 def compute_bonus(ees_loc):
-    for i in range(1, len(ees_loc)):
+    # Compare current number with the number on the left
+    for i in range(1, len(ees_loc)): # Add 1-bonus if the number is higher
         if ees_loc[i] > ees_loc[i-1]:
-            ees_b[i] = ees_b[i-1] + 1
+            ees_b[i] += 1
         elif ees_loc[i] < ees_loc[i-1]:
-            for j in range(i,0,-1):
+            for j in range(i,0,-1): # Reverse a loop, if 2 numbers have the same value, add 1-bonus to the number on the left
                 if ees_b[j] == ees_b[j-1]:
-                    ees_b[j-1] +=1
+                    ees_b[j-1] += 1
     return print(f'Employees lines of code: {ees_loc} \nEmployees bonus: {ees_b}')
 
 ees_ls = list(input('Enter lines of codes for each employee (seperated by comma): ').split(','))
