d6fd12d8b34567282538543a614d289e0f5cffcc
wiehoang <ndhhoang1210@gmail.com>
2024-03-04 03:02:16 +0700

    Update alphabetic_pattern.py

--- alphabetic_pattern.py
+++ alphabetic_pattern.py
@@ -13,14 +13,14 @@
 def print_pattern(n):
     for row in range(1, 2 * n):
 
-        # Calculate number of half-side dashes on each row
+        # Calculate a number of half-side dashes on each row
         num_of_dashes: int
         if row <= n:
             num_of_dashes = (n - row) * 2
         else:
             num_of_dashes = (row - n) * 2
 
-        # Calculate number of half-left chars on each row
+        # Calculate a number of half-left chars on each row
         num_of_hl_chars: int
         if row <= n:
             num_of_hl_chars = row
@@ -28,25 +28,25 @@ def print_pattern(n):
             num_of_hl_chars = row - num_of_dashes
 
         # Assign an ASCII value of character
-        char_val: int = 97 + n - 1  # 97 is ASCII code of starting letter 'a'
+        char_val: int = 97 + n - 1  # 97 is an ASCII code of starting letter 'a'
 
         # Print half-left dashes
-        for dash in range(num_of_dashes):
+        for hld in range(num_of_dashes):
             print('-', end = '')
 
         # Print half-left chars
-        for char in range(num_of_hl_chars):
+        for hlc in range(num_of_hl_chars):
             print(chr(char_val), end = '-')
             char_val -= 1
 
         # Print half-right chars
         upd_char_val = char_val + 2  # Assign a new char_val to print symmetrically with the half-left chars
-        for char in range(num_of_hl_chars - 1):
+        for hrc in range(num_of_hl_chars - 1):
             print(chr(upd_char_val), end = '-')
             upd_char_val += 1
 
-        # Print half right dashes
-        for space in range(num_of_dashes - 1):
+        # Print half-right dashes
+        for hrd in range(num_of_dashes - 1):
             print('-', end = '')
 
         # Add a new line after printed a row
